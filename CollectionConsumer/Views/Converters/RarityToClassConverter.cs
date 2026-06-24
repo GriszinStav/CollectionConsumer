a4661471f7b60bc4507e2590bd0ac3c76ee724a4
Slipknot <Slipknot@hell.com>
2026-06-24 13:30:25 +0300

    build_1.3_better_rarities

--- CollectionConsumer/Views/Converters/RarityToClassConverter.cs
+++ CollectionConsumer/Views/Converters/RarityToClassConverter.cs
@@ -11,20 +11,25 @@ namespace CollectionConsumer.Views.Converters
         {
             if (value is string rarity)
             {
-                return rarity.ToLower() switch
+                var color = rarity.ToLower() switch
                 {
-                    "common" => new SolidColorBrush(Colors.LightGray),
-                    "uncommon" => new SolidColorBrush(Colors.LightBlue),
-                    "rare" => new SolidColorBrush(Color.FromRgb(129, 212, 250)),
-                    "special" => new SolidColorBrush(Color.FromRgb(206, 147, 216)),
-                    "epic" => new SolidColorBrush(Color.FromRgb(179, 157, 219)),
-                    "extraordinary" => new SolidColorBrush(Color.FromRgb(144, 202, 249)),
-                    "mythic" => new SolidColorBrush(Color.FromRgb(255, 213, 79)),
-                    "legendary" => new SolidColorBrush(Color.FromRgb(255, 171, 145)),
-                    _ => new SolidColorBrush(Colors.LightGray)
+                    "common" => Colors.Gray,
+                    "uncommon" => Colors.LightBlue,
+                    "rare" => Color.FromRgb(129, 212, 250),
+                    "special" => Color.FromRgb(206, 147, 216),
+                    "epic" => Color.FromRgb(179, 157, 219),
+                    "extraordinary" => Color.FromRgb(144, 202, 249),
+                    "mythic" => Color.FromRgb(245, 39, 108),
+                    "legendary" => Color.FromRgb(245, 73, 39),
+                    _ => Colors.Gray
                 };
+
+                if (parameter?.ToString() == "Shadow")
+                    return color;
+
+                return new SolidColorBrush(color);
             }
-            return new SolidColorBrush(Colors.LightGray);
+            return new SolidColorBrush(Colors.Gray);
         }
 
         public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
