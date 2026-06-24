419a1b36ccc917e5333bf39be7bb1fd43868810e
Slipknot <Slipknot@hell.com>
2026-06-24 15:06:35 +0300

    build_1.4_icon

---
+++ CollectionConsumer/Views/Converters/BooleanToColorConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+using Avalonia.Media;
+
+namespace CollectionConsumer.Views.Converters
+{
+    public class BooleanToColorConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is bool isDescending)
+                return isDescending
+                    ? new SolidColorBrush(Color.FromRgb(33, 150, 243))
+                    : new SolidColorBrush(Colors.Gray);
+            return new SolidColorBrush(Colors.Gray);
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
